62fa1018f238e870c214e26e7e777d53e22bd880
Shreyaa Katiyar <shreyaakatiyar@gmail.com>
2026-03-09 22:22:36 +0530

    update work experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,24 +6,26 @@ const Experience = () => {
     <>
       <section className="py-24 px-6 bg-slate-950/50" id="experience">
         <div className="max-w-4xl mx-auto">
-          <h2 class="text-4xl md:text-5xl font-bold text-white leading-tight italic text-center pb-16">
+          <h2 class="text-4xl md:text-5xl font-bold text-white leading-tight text-center pb-16">
             Work Experience
           </h2>
 
-          <div className="space-y-8 relative before:absolute before:inset-0 before:ml-5 md:before:mx-auto before:-translate-x-px md:before:translate-x-0 before:h-full before:w-0.5 before:bg-linear-to-b before:from-transparent before:via-primary/30 before:to-transparent">
-            <div class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group">
-              <div class="flex items-center justify-center w-10 h-10 rounded-full border border-[#8c2bee] bg-[#0a0510] text-primary absolute left-0 md:left-1/2 md:-translate-x-1/2 z-10  backdrop-blur-md p-6 group-hover:scale-110 transition-transform">
-                <PiBuildingOfficeBold />
+          <div className="space-y-8 relative before:absolute before:inset-0 before:ml-5 md:before:mx-auto before:-translate-x-px md:before:translate-x-0 before:h-full before:w-0.5 before:bg-linear-to-b before:from-transparent before:via-[#8c2bee]/30 before:to-transparent">
+            <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group">
+              <div className="flex items-center justify-center w-10 h-10 rounded-full border border-[#8c2bee] bg-[#0a0510] text-[#8c2bee] absolute left-0 md:left-1/2 md:-translate-x-1/2 z-10  backdrop-blur-md p-6 group-hover:scale-110 transition-transform">
+                <span className="material-symbols-outlined text-sm">
+                    <PiBuildingOfficeBold />
+                </span>
               </div>
-              <div class="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] glass p-6 rounded-2xl border border-primary/10 hover:border-primary/40 transition-all">
-                <div class="flex items-center justify-between mb-2">
-                  <h3 class="font-bold text-xl text-primary">Intern</h3>
-                  <time class="text-xs text-slate-500 font-mono">
+              <div class="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-[rgba(25,16,34,0.7)] backdrop-blur-md border-[rgba(140,43,238,0.2)] p-6 rounded-2xl border hover:border-[#8c2bee]/40 transition-all">
+                <div className="flex items-center justify-between mb-2">
+                  <h3 className="font-bold text-xl text-[#8c2bee]">Intern</h3>
+                  <time className="text-xs text-slate-500 font-mono">
                     April 2025 - July 2025
                   </time>
                 </div>
-                <p class="text-slate-300 font-medium mb-3">SoftHire</p>
-                <ul class="text-sm text-slate-500 space-y-2 list-disc list-inside">
+                <p className="text-slate-300 font-medium mb-3">SoftHire</p>
+                <ul className="text-sm text-slate-500 space-y-2 list-disc list-inside">
                   <li>
                     Designed and developed user-friendly interfaces for web applications, focusing on improving usability, visual consistency, and overall user experience.
                   </li>
@@ -34,19 +36,21 @@ const Experience = () => {
               </div>
             </div>
 
-            <div class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group">
-              <div class="flex items-center justify-center w-10 h-10 rounded-full border border-primary/50 bg-background-dark text-primary absolute left-0 md:left-1/2 md:-translate-x-1/2 z-10 glass group-hover:scale-110 transition-transform">
-                <span class="material-symbols-outlined text-sm">palette</span>
+            <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group">
+              <div className="flex items-center justify-center w-10 h-10 rounded-full border border-[#8c2bee] bg-[#0a0510] text-[#8c2bee] absolute left-0 md:left-1/2 md:-translate-x-1/2 z-10  backdrop-blur-md p-6 group-hover:scale-110 transition-transform">
+                <span className="material-symbols-outlined text-sm">
+                    <PiBuildingOfficeBold />
+                </span>
               </div>
-              <div class="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] glass p-6 rounded-2xl border border-primary/10 hover:border-primary/40 transition-all">
-                <div class="flex items-center justify-between mb-2">
-                  <h3 class="font-bold text-xl text-primary">UI/UX Designer</h3>
-                  <time class="text-xs text-slate-500 font-mono">
+              <div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-[rgba(25,16,34,0.7)] backdrop-blur-md border-[rgba(140,43,238,0.2)] p-6 rounded-2xl border hover:border-[#8c2bee]/40 transition-all">
+                <div className="flex items-center justify-between mb-2">
+                  <h3 className="font-bold text-xl text-[#8c2bee]">UI/UX Designer</h3>
+                  <time className="text-xs text-slate-500 font-mono">
                     2022 - 2023
                   </time>
                 </div>
-                <p class="text-slate-300 font-medium mb-3">Programming Club</p>
-                <ul class="text-sm text-slate-500 space-y-2 list-disc list-inside">
+                <p className="text-slate-300 font-medium mb-3">Programming Club</p>
+                <ul className="text-sm text-slate-500 space-y-2 list-disc list-inside">
                   <li>
                     Led design for internal tools and hackathon platforms.
                   </li>
@@ -55,21 +59,23 @@ const Experience = () => {
               </div>
             </div>
 
-            <div class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group">
-              <div class="flex items-center justify-center w-10 h-10 rounded-full border border-primary/50 bg-background-dark text-primary absolute left-0 md:left-1/2 md:-translate-x-1/2 z-10 glass group-hover:scale-110 transition-transform">
-                <span class="material-symbols-outlined text-sm">groups</span>
+            <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group">
+              <div className="flex items-center justify-center w-10 h-10 rounded-full border border-[#8c2bee] bg-[#0a0510] text-[#8c2bee] absolute left-0 md:left-1/2 md:-translate-x-1/2 z-10  backdrop-blur-md p-6 group-hover:scale-110 transition-transform">
+                <span className="material-symbols-outlined text-sm">
+                    <PiBuildingOfficeBold />
+                </span>
               </div>
-              <div class="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] glass p-6 rounded-2xl border border-primary/10 hover:border-primary/40 transition-all">
-                <div class="flex items-center justify-between mb-2">
-                  <h3 class="font-bold text-xl text-primary">Designer</h3>
-                  <time class="text-xs text-slate-500 font-mono">
+              <div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-[rgba(25,16,34,0.7)] backdrop-blur-md border-[rgba(140,43,238,0.2)] p-6 rounded-2xl border hover:border-[#8c2bee]/40 transition-all">
+                <div className="flex items-center justify-between mb-2">
+                  <h3 className="font-bold text-xl text-[#8c2bee]">Designer</h3>
+                  <time className="text-xs text-slate-500 font-mono">
                     2021 - 2022
                   </time>
                 </div>
-                <p class="text-slate-300 font-medium mb-3">
+                <p className="text-slate-300 font-medium mb-3">
                   Computer Society of India (CSI)
                 </p>
-                <ul class="text-sm text-slate-500 space-y-2 list-disc list-inside">
+                <ul className="text-sm text-slate-500 space-y-2 list-disc list-inside">
                   <li>
                     Designed visual identities for tech workshops and seminars.
                   </li>
