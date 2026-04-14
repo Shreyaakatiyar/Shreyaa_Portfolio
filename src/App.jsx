603d5143c1a2e492190bfd8e6b9a8ef99935aa2b
Shreyaa Katiyar <shreyaakatiyar@gmail.com>
2026-04-15 00:48:20 +0530

    update imports in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { motion } from "motion/react"
 import { useState } from 'react'
 import './App.css'
-import Home from "./pages/home"
+import Home from "./pages/Home"
 
 function App() {
   const [count, setCount] = useState(0)
